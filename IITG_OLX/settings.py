c012cdf38af16e12ac5c24ddefa336e1a70b14c0
ShikharBind <shikhar.bind2010@gmail.com>
2023-11-04 19:59:03 +0530

    allowed all host

--- IITG_OLX/settings.py
+++ IITG_OLX/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-@8zs5y@wnr)ybesu*s1f2y6!8+bt&%j1!8fpgwvr^ixe046z+7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 # DEBUG = False
-# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
